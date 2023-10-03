de555bb870a01eeaad9917917f8eb609b791d702
Tyuiu.SabarovDA@outlook.com <Tyuiu.SabarovDA@outlook.com>
2023-10-03 22:02:05 +0300

    Add project files.

---
+++ Tyuiu.SabarovDA.Sprint2.Task0.V3.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.SabarovDA.Sprint2.Task0.V3.Lib
+{ //45 127 false, true, false, true, false, true
+    public class DataService : ISprint2Task0V3
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x == y;
+            res[1] = x != y;
+            res[2] = x + 100 < y;
+            res[3] = x + 100 > y;
+            res[4] = x + 100 <= y;
+            res[5] = x + 100 >= y;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SabarovDA.Sprint2.Task0.V3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SabarovDA.Sprint2.Task0.V3.Lib;
+namespace Tyuiu.SabarovDA.Sprint2.Task0.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 45;
+            int y = 127;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.Title = "Спринт #2 | Выполнил: Сабаров Д. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Сабаров Дмитрий Алексанрдович | ИИПб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("*  последовательность операций не должна нарушаться) и арифметических     *");
+            Console.WriteLine("*  выражений, которая вернет логическую последовательность(массив):       *");
+            Console.WriteLine("*  (False, True, False, True, False, True), при x = 45, y = 127           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение x =" + x);
+            Console.WriteLine("Значение y =" + y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            for (int i=0; i<6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
