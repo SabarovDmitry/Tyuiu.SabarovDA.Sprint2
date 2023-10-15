3542416881a535ab4395ea500cbfbdf03d4b8883
Tyuiu.SabarovDA@outlook.com <Tyuiu.SabarovDA@outlook.com>
2023-10-15 21:35:03 +0300

    Task7

---
+++ Tyuiu.SabarovDA.Sprint2.Task7.V7.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SabarovDA.Sprint2.Task7.V7.Lib;
+namespace Tyuiu.SabarovDA.Sprint2.Task7.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadeArea()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.1;
+            double y = 0.7;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SabarovDA.Sprint2.Task7.V7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SabarovDA.Sprint2.Task7.V7.Lib;
+namespace Tyuiu.SabarovDA.Sprint2.Task7.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Сабаров Д. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Сабаров Дмитрий Александрович | ИИПб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные           *");
+            Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка с координатами  *");
+            Console.WriteLine("* X,Y в заштрихованной области.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка входит в заданную область");
+            }
+            else
+            {
+                Console.WriteLine("Точка НЕ входит в заданную область");
+            }
+            Console.ReadKey();
+        }
+    }
+}
