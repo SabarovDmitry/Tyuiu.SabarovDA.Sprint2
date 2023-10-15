19a7fb1bd806490794fcad9d75c5383312787405
Tyuiu.SabarovDA@outlook.com <Tyuiu.SabarovDA@outlook.com>
2023-10-15 21:06:32 +0300

    Task6

---
+++ Tyuiu.SabarovDA.Sprint2.Task6.V7.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.SabarovDA.Sprint2.Task6.V7.Lib
+{
+    public class DataService : ISprint2Task6V7
+    {
+        public string FindMonthName(int startYear, int n)
+        {
+            while (n >= 12)
+            {
+                n -= 12;
+            }
+
+            switch (n + 1)
+            {
+                case 1: return "Январь";
+                case 2: return "Февраль";
+                case 3: return "Март";
+                case 4: return "Апрель";
+                case 5: return "Май";
+                case 6: return "Июнь";
+                case 7: return "Июль";
+                case 8: return "Август";
+                case 9: return "Сентябрь";
+                case 10: return "Октябрь";
+                case 11: return "Ноябрь";
+                case 12: return "Декабрь";
+                default: throw new ArgumentException($"Месяц должен быть от 1 до 12. Введенное значение {n}");
+            }
+
+        }
+    }
+}
---
+++ Tyuiu.SabarovDA.Sprint2.Task6.V7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SabarovDA.Sprint2.Task6.V7.Lib;
+namespace Tyuiu.SabarovDA.Sprint2.Task6.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Сабаров Д. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Сабаров Дмитрий Александрович | ИИПб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch, вычисляет       *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("* Условие: С начала 1990 года по некоторый день прошло n месяцев и 2 дня. *");
+            Console.WriteLine("* Определить название месяца (январь, февраль и т. п.) этого дня.         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Укажите с какого года ведется отсчет: ");
+            int startYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Укажите сколько месяцев прошло: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"С начала {startYear} года прошло {n} и 2 дня: {ds.FindMonthName(startYear, n)}");
+
+            Console.ReadKey();
+        }
+    }
+}
