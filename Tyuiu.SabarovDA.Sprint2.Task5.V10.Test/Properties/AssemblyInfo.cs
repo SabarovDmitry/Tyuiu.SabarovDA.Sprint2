b887454a21bad26ad6d85fe1b500d58e4c6f7253
Tyuiu.SabarovDA@outlook.com <Tyuiu.SabarovDA@outlook.com>
2023-10-15 20:48:11 +0300

    Task5

---
+++ Tyuiu.SabarovDA.Sprint2.Task5.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SabarovDA.Sprint2.Task5.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SabarovDA.Sprint2.Task5.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e860e498-da51-4e2c-b2b6-6a0b625cc901")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SabarovDA.Sprint2.Task5.V10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SabarovDA.Sprint2.Task5.V10.Lib;
+namespace Tyuiu.SabarovDA.Sprint2.Task5.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Сабаров Д. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Сабаров Дмитрий Александрович | ИИПб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие: Дата некоторого дня характеризуется тремя натуральными числами:*");
+            Console.WriteLine("* g (год), m (порядковый номер месяца) и n (число). По заданным g, n и m  *");
+            Console.WriteLine("* определить дату предыдущего дня. Заданный год не является високосным.   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите год:");
+            int g = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите месяц:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите день:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Результат: " + ds.FindDateOfPreviousDay(g, m, n));
+            Console.ReadKey();
+        }
+    }
+}
